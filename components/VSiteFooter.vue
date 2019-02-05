fe6de8ed933924d087655803b1fe381de30fdce6
David Royer <droyer01@gmail.com>
2019-02-05 03:27:58 -0500

    fix: Fixes social icons by removing them

--- components/VSiteFooter.vue
+++ components/VSiteFooter.vue
@@ -10,7 +10,7 @@
       flat
       tile
     >
-      <v-card-text>
+      <!-- <v-card-text>
         <v-btn
           v-for="icon in icons"
           :key="icon"
@@ -21,7 +21,7 @@
             {{ icon }}
           </v-icon>
         </v-btn>
-      </v-card-text>
+      </v-card-text> -->
 
       <v-card-text class="white--text pt-0">
         Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui.
