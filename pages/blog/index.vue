58929c825b1962e0b1ab1a2bf0e57804f509c0da
David Royer <droyer01@gmail.com>
2019-02-07 21:43:12 -0500

    chore: eslint code fix

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nuxt-link v-for="(post, index) in posts" :key="index" :to="`/blog/${post.attributes.name}`" v-text="post.attributes.title" class="title"/>
+    <nuxt-link v-for="(post, index) in posts" :key="index" :to="`/blog/${post.attributes.name}`" class="title" v-text="post.attributes.title" />
     <v-divider class="my-4" />
     <pre>{{ posts }}</pre>
   </div>
