d29b25011c504c9f98364800eef88e596a116827
David Royer <droyer01@gmail.com>
2019-04-01 09:17:51 -0400

    fix: sets correct primary color

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,13 +4,14 @@ import Vuetify from 'vuetify/lib'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#40526b', // a color that is not in the material colors palette
+    primary: '#373a47', // a color that is not in the material colors palette
     accent: '#01d4a7',
     secondary: colors.grey.darken3,
     info: colors.teal.lighten1,
     warning: colors.amber.base,
     error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+    success: colors.green.accent3,
+    customName: colors.blueGrey.darken4
   },
   options: {
     customProperties: true
